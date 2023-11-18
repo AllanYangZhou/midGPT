ac8cd5b61d25b862cd246776eaae7db2e652ef7e
Allan <8732147+AllanYangZhou@users.noreply.github.com>
2023-11-18 00:59:45 -0800

    Block

--- model.py
+++ model.py
@@ -12,8 +12,8 @@ class MLP(eqx.Module):
 
     def __init__(self, n_embd, bias, dropout, key):
         key1, key2 = jrandom.split(key)
-        self.c_fc = jax.vmap(eqx.nn.Linear(n_embd, 4 * n_embd, use_bias=bias, key=key1))
-        self.c_proj = jax.vmap(eqx.nn.Linear(4 * n_embd, n_embd, use_bias=bias, key=key2))
+        self.c_fc = eqx.nn.Linear(n_embd, 4 * n_embd, use_bias=bias, key=key1)
+        self.c_proj = eqx.nn.Linear(4 * n_embd, n_embd, use_bias=bias, key=key2)
         self.dropout = eqx.nn.Dropout(dropout)
 
     def __call__(self, x, key):
@@ -33,42 +33,56 @@ class CausalSelfAttention(eqx.Module):
         key1, key2 = jrandom.split(key)
         assert n_embd % n_head == 0
         self.n_head, self.n_embd = n_head, n_embd
-        self.c_attn = jax.vmap(jax.vmap(
-            eqx.nn.Linear(n_embd, 3 * n_embd, use_bias=bias, key=key1)))
-        self.c_proj = jax.vmap(jax.vmap(
-            eqx.nn.Linear(n_embd, n_embd, use_bias=bias, key=key2)))
+        self.c_attn = eqx.nn.Linear(n_embd, 3 * n_embd, use_bias=bias, key=key1)
+        self.c_proj = eqx.nn.Linear(n_embd, n_embd, use_bias=bias, key=key2)
         self.attn_dropout = eqx.nn.Dropout(dropout)
         self.resid_dropout = eqx.nn.Dropout(dropout)
 
     def __call__(self, x, key):
         key1, key2 = jrandom.split(key)
-        B, T, C = x.shape
-        qkv = self.c_attn(x)  # (B, T, 3 * C)
-        q, k, v = jnp.split(qkv, 3, axis=-1)  # (B, T, C)
+        T, C = x.shape
+        qkv = jax.vmap(self.c_attn)(x)  # (T, 3 * C)
+        q, k, v = jnp.split(qkv, 3, axis=-1)  # (T, C)
         n_per_head = self.n_embd // self.n_head
-        # (B, T, C) -> (B, n_head, T, n_per_head)
-        q = jnp.transpose(jnp.reshape(q, (B, T, self.n_head, n_per_head)), (0, 2, 1, 3))
-        k = jnp.transpose(jnp.reshape(k, (B, T, self.n_head, n_per_head)), (0, 2, 1, 3))
-        v = jnp.transpose(jnp.reshape(v, (B, T, self.n_head, n_per_head)), (0, 2, 1, 3))
-        att = q @ jnp.transpose(k, (0, 1, 3, 2))  # (B, n_head, T, T)
+        # (T, C) -> (n_head, T, n_per_head)
+        q = jnp.transpose(jnp.reshape(q, (T, self.n_head, n_per_head)), (1, 0, 2))
+        k = jnp.transpose(jnp.reshape(k, (T, self.n_head, n_per_head)), (1, 0, 2))
+        v = jnp.transpose(jnp.reshape(v, (T, self.n_head, n_per_head)), (1, 0, 2))
+        att = q @ jnp.transpose(k, (0, 2, 1))  # (n_head, T, T)
         # Causal mask
         idx_x, idx_y = jnp.triu_indices(T, 0)
         att = att.at[..., idx_x, idx_y].set(float('-inf'))
         att = jax.nn.softmax(att / jnp.sqrt(n_per_head), axis=-1)
         att = self.attn_dropout(att, key=key1)
-        out = jnp.reshape(jnp.transpose(att @ v, (0, 2, 1, 3)), (B, T, C))
-        return self.resid_dropout(self.c_proj(out), key=key2)
+        out = jnp.reshape(jnp.transpose(att @ v, (1, 0, 2)), (T, C))
+        return self.resid_dropout(jax.vmap(self.c_proj)(out), key=key2)
+
+
+class Block(eqx.Module):
+    attn: CausalSelfAttention
+    mlp: MLP
+    ln1: eqx.Module
+    ln2: eqx.Module
+    def __init__(self, n_embd, n_head, bias, dropout, key):
+        self.attn = CausalSelfAttention(
+            n_embd=n_embd, n_head=n_head, bias=bias, dropout=dropout, key=key)
+        self.mlp = MLP(n_embd=n_embd, bias=bias, dropout=dropout, key=key)
+        self.ln1 = eqx.nn.LayerNorm(n_embd, eps=1e-5, use_bias=bias)
+        self.ln2 = eqx.nn.LayerNorm(n_embd, eps=1e-5, use_bias=bias)
+
+    def __call__(self, x, key):
+        ln1, ln2 = jax.vmap(self.ln1), jax.vmap(self.ln2)
+        key1, key2 = jrandom.split(key)
+        x = x + self.attn(ln1(x), key1)
+        key2 = jrandom.split(key2, x.shape[0])
+        return x + jax.vmap(self.mlp)(ln2(x), key2)
 
 
 if __name__ == "__main__":
     key = jrandom.PRNGKey(0)
-    key, key1, key2 = jrandom.split(key, 3)
-    causal_sa = CausalSelfAttention(512, 8, False, 0.1, key=key1)
+    key, key1 = jrandom.split(key)
+    block = Block(512, 8, False, 0.1, key1)
     inp = jnp.ones((10, 100, 512))
-    out = causal_sa(inp, key1)
-    print(inp.shape, out.shape)
-    key, key1, key2 = jrandom.split(key, 3)
-    mlp = MLP(100, False, 0.1, key1)
-    inp = jnp.ones((10, 100))
-    out = mlp(inp, key2)
+    key, subkey = jrandom.split(key)
+    out = jax.vmap(block)(inp, jrandom.split(subkey, inp.shape[0]))
     print(inp.shape, out.shape)
