65d8328bd910fcd182001cfd01459f25ecb21164
Allan Zhou <8732147+AllanYangZhou@users.noreply.github.com>
2023-11-25 21:55:10 +0000

    Scan over g_accum_iters

--- src/train.py
+++ src/train.py
@@ -31,7 +31,7 @@ class ExperimentConfig:
     warmup_steps: int
     min_lr: float  # Final LR after decay
     lr_decay_steps: int
-    max_steps: int  # No. of grad steps: iters / g_accum_iters
+    max_steps: int  # No. of grad steps
     beta2: float
     weight_decay: float
     eval_interval: int
@@ -42,10 +42,16 @@ class ExperimentConfig:
     debug: bool = False
 
 
-def get_batch(data, block_size: int, batch_size: int) -> tp.Tuple[np.ndarray, np.ndarray]:
-    ix = np.random.randint(0, len(data) - block_size, size=(batch_size,))
+def get_batch(
+        data, block_size: int, batch_size: int, g_accum_iters: tp.Optional[int]=None
+) -> tp.Tuple[np.ndarray, np.ndarray]:
+    bs = batch_size * (g_accum_iters or 1)
+    ix = np.random.randint(0, len(data) - block_size, size=(bs,))
     x = np.take(data, np.arange(block_size) + ix[:, None], axis=0).astype(np.int32)
     y = np.take(data, np.arange(1, block_size + 1) + ix[:, None], axis=0).astype(np.int32)
+    if g_accum_iters is not None:  # reshape to (g_accum_steps, batch_size, block_size)
+        x = x.reshape(g_accum_iters, batch_size, block_size)
+        y = y.reshape(g_accum_iters, batch_size, block_size)
     return x, y
 
 
@@ -53,7 +59,8 @@ def make_training_fns(
         config: ExperimentConfig, optimizer: optax.GradientTransformationExtraArgs,
         mesh: Mesh, shard_model: bool) -> tp.Tuple[tp.Callable, tp.Callable]:
     policy = jmp.get_policy(config.policy)
-    def loss_fn(model: GPT, x: Array, y: Array, key: tp.Optional[KeyArray]) -> Array:
+    def loss_fn(model_params: GPT, model_static: GPT, x: Array, y: Array, key: tp.Optional[KeyArray]) -> Array:
+        model = eqx.combine(model_params, model_static)
         if key is not None:
             key = jrandom.split(key, x.shape[0])
         logits = vmap(model)(x, key=key)
@@ -64,16 +71,34 @@ def make_training_fns(
 
     @partial(eqx.filter_jit, donate='all')
     def step(model: GPT, opt_state, x: Array, y: Array, key: KeyArray):
-        model_half = policy.cast_to_compute(model)
-        loss, grad = eqx.filter_value_and_grad(loss_fn)(model_half, x, y, key)
-        grad = shard_gpt(grad, mesh, shard_model)
-        grad = policy.cast_to_param(grad)
+        G = config.g_accum_iters
+        # put params in compute dtype (probably bfloat16), and split params out
+        model_params, model_static = eqx.partition(policy.cast_to_compute(model), eqx.is_array)
+        # compute loss and grad and microbatch g, then scan over microbatches.
+        def accum_loss_grad(loss_and_grad, xykey_g: tp.Tuple[Array, Array, KeyArray]):
+            loss_so_far, grad_so_far = loss_and_grad
+            loss_g, grad_g = jax.value_and_grad(loss_fn)(model_params, model_static, *xykey_g)
+            loss_so_far = loss_so_far + loss_g
+            grad_so_far = jtu.tree_map(lambda x, y: x + y, grad_g, grad_so_far)
+            return (loss_so_far, grad_so_far), None
+        all_keys = jrandom.split(key, config.g_accum_iters)
+        init_loss_grad = (jnp.zeros(()), jax.tree_map(jnp.zeros_like, model_params))
+        (loss, grad), _ = scan(accum_loss_grad, init_loss_grad, (x, y, all_keys))
+        # Loss and grad were accumulated (summed) over G, so divide.
+        loss, grad = loss / G, jtu.tree_map(lambda x: x / G, grad)
+        # put grad back in params dtype and enforce sharding
+        grad = shard_gpt(policy.cast_to_param(grad), mesh, shard_model)
         updates, opt_state = optimizer.update(grad, opt_state, model)
         model = eqx.apply_updates(model, updates)
         return model, opt_state, loss
 
+    @eqx.filter_jit
+    def simple_loss(model: GPT, x: Array, y: Array, key: tp.Optional[KeyArray]) -> Array:
+        """Same as loss_fn, but doesn't split params into compute/static."""
+        model_params, model_static = eqx.partition(model, eqx.is_array)
+        return loss_fn(model_params, model_static, x, y, key)
+
     data_sharding = NamedSharding(mesh, P('data', None))
-    fast_loss_fn = eqx.filter_jit(loss_fn)
     def evaluate(model: GPT, data: np.ndarray) -> Array:
         model = policy.cast_to_compute(model)
         model = eqx.Partial(model, inference=True)
@@ -81,7 +106,7 @@ def make_training_fns(
         for i in range(200):
             x, y = get_batch(data, config.model_config.block_size, config.batch_size)
             x, y = jax.device_put((x, y), data_sharding)
-            loss = fast_loss_fn(model, x, y, None)
+            loss = simple_loss(model, x, y, None)
             tot_loss = tot_loss + loss
         return tot_loss / 200
 
@@ -141,17 +166,15 @@ def train(config: ExperimentConfig):
         options=options)
 
     # optax operates on iters, not grad steps
-    warmup_iters = config.warmup_steps * config.g_accum_iters
-    lr_decay_iters = config.lr_decay_steps * config.g_accum_iters
     scheduler = optax.warmup_cosine_decay_schedule(
-        0, config.learning_rate, warmup_iters, lr_decay_iters, end_value=config.min_lr)
+        0, config.learning_rate, config.warmup_steps, config.lr_decay_steps,
+        end_value=config.min_lr)
     optimizer = optax.chain(
         optax.clip_by_global_norm(1.0),
         optax.scale_by_adam(b2=config.beta2),
         optax.add_decayed_weights(config.weight_decay),
         optax.scale_by_schedule(scheduler),
         optax.scale(-1),
-        optax.apply_every(config.g_accum_iters),
     )
     step, evaluate = make_training_fns(config, optimizer, mesh, config.shard_model)
 
@@ -170,13 +193,11 @@ def train(config: ExperimentConfig):
         model = jtu.tree_unflatten(jtu.tree_structure(model), model_leaves)
         opt_state = jtu.tree_unflatten(jtu.tree_structure(opt_state), opt_state_leaves)
         first_step = mngr.latest_step() + 1
-    data_sharding = NamedSharding(mesh, P('data', None))
+    data_sharding = NamedSharding(mesh, P(None, 'data', None))  # (G, BS, d)
     postfix_values = {}  # values to display in the progress bar
-    max_iters = config.max_steps * config.g_accum_iters
-    pbar = trange(first_step, max_iters, initial=first_step, total=max_iters)
-    eval_interval = config.eval_interval * config.g_accum_iters
+    pbar = trange(first_step, config.max_steps, initial=first_step, total=config.max_steps)
     for itr in pbar:
-        if not config.debug and (itr % eval_interval == 0):
+        if not config.debug and (itr % config.eval_interval == 0):
             train_loss = evaluate(model, train_data).item()
             val_loss = evaluate(model, val_data).item()
             postfix_values['train_loss'] = train_loss
@@ -184,7 +205,9 @@ def train(config: ExperimentConfig):
             writer.add_scalar('loss/train', train_loss, itr)
             writer.add_scalar('loss/val', val_loss, itr)
         key, key1 = jrandom.split(key)
-        x, y = get_batch(train_data, config.model_config.block_size, config.batch_size)
+        x, y = get_batch(
+            train_data, config.model_config.block_size, config.batch_size, config.g_accum_iters
+        )
         if itr == 1: jax.profiler.start_trace(os.path.join(config.rundir, 'logs'))
         x, y = jax.device_put((x, y), data_sharding)
         model, opt_state, loss = step(model, opt_state, x, y, key1)
@@ -193,7 +216,7 @@ def train(config: ExperimentConfig):
         postfix_values['loss'] = loss.item()
         postfix_values['lr'] = scheduler(opt_state[3].count).item()
         if pbar.format_dict['rate'] is not None:
-            postfix_values['thruput'] = pbar.format_dict['rate'] * config.batch_size
+            postfix_values['thpt'] = pbar.format_dict['rate'] * config.batch_size * config.g_accum_iters
         pbar.set_postfix(**postfix_values)
     pbar.close()
     writer.close()
