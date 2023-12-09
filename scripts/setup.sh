5f4caabe67b171d21cdb6c01bbf0d5c934acbf88
Allan Zhou <8732147+AllanYangZhou@users.noreply.github.com>
2023-12-09 15:16:08 -0800

    modify setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,15 @@
-# gcloud compute tpus tpu-vm scp setup.sh node3: --worker=all --zone=europe-west4-a
-# gcloud compute tpus tpu-vm ssh node3 --zone=europe-west4-a --worker=all --command="chmod +x ./setup.sh; ./setup.sh"    
+#!/bin/sh
 
-pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-pip install equinox==0.11.2 tiktoken==0.5.1 optax==0.1.7 tqdm==4.66.1 transformers==4.35.2 tiktoken==0.5.1 orbax==0.1.9 rich==13.7.0 tensorboard==2.15.1 tensorboardX==2.6.2.2
-pip install tensorflow-cpu tensorboard-plugin-profile
+source scripts/tpu_commands.sh
+
+tpu midGPT copy node3
+gcloud compute tpus tpu-vm ssh node3 --zone=europe-west4-a --worker=all --command="pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html"
+gcloud compute tpus tpu-vm ssh node3 --zone=europe-west4-a --worker=all --command="cd midGPT; pip install -r requirements.txt"
+
+# Attach and mount PD that has dataset.
+gcloud alpha compute tpus tpu-vm attach-disk node3 \
+  --zone=europe-west4-a \
+  --disk=owt \
+  --mode=read-only
+sudo mkdir -p /mnt/disks/persist
+sudo mount -o discard,defaults /dev/sdb /mnt/disks/persist
