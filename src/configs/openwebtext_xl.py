2245d29c69c06e522c58ee624a6831cd965566c3
Allan Zhou <8732147+AllanYangZhou@users.noreply.github.com>
2023-12-11 20:24:46 -0600

    update configs

--- src/configs/openwebtext_xl.py
+++ src/configs/openwebtext_xl.py
@@ -4,19 +4,19 @@ from src.model import GPTConfig
 config = ExperimentConfig(
     rundir='',
     data_dir='/mnt/disks/persist/openwebtext',
-    learning_rate=6e-4,
-    batch_size=120,
+    learning_rate=2e-4,
+    batch_size=1024,
     warmup_steps=5000,
-    min_lr=1e-5,
-    lr_decay_steps=600_000,
-    max_steps=600_000,
+    min_lr=2e-5,
+    lr_decay_steps=60_000,
+    max_steps=60_000,
     beta2=0.95,
-    weight_decay=6e-5,
+    weight_decay=1e-4,
     eval_interval=1000,
     compute_dtype='bfloat16',
     param_dtype='float32',
-    g_accum_iters=4,
-    shard_model=False,
+    g_accum_iters=1,
+    shard_model=True,
     model_config=GPTConfig(
-        block_size=1024, vocab_size=50304, n_layer=12, n_head=12, n_embd=768, dropout=0.0)
+        block_size=1024, vocab_size=50304, n_layer=24, n_head=16, n_embd=2048, dropout=0.0)
 )
