9009a4c79a5150e357e7cd7cbf19e5ced6ec6fe8
Nick Landolfi <nicholas.charles@landolfi.org>
2023-12-13 14:52:57 -0800

    update setup.sh to take node, zone, disk as arguments

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,17 +3,17 @@
 source scripts/tpu_commands.sh
 
 # Remove any outdated info from known hosts.
-for ip in $(tpu midGPT ips node3); do ssh-keygen -R $ip; done
+for ip in $(tpu midGPT ips $2); do ssh-keygen -R $ip; done
 
-tpu midGPT copy node3
-tpu midGPT ssh node3 "pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html"
-tpu midGPT ssh node3 "cd midGPT; pip install -r requirements.txt"
+tpu midGPT copy $2
+tpu midGPT ssh $2 "pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html"
+tpu midGPT ssh $2 "cd midGPT; pip install -r requirements.txt"
 
 # Attach and mount PD that has dataset.
-gcloud alpha compute tpus tpu-vm attach-disk node3 \
-  --zone=europe-west4-a \
-  --disk=owt \
+gcloud alpha compute tpus tpu-vm attach-disk $2 \
+  --zone=$1 \
+  --disk=$3 \
   --mode=read-only
 
-tpu midGPT ssh node3 "sudo mkdir -p /mnt/disks/persist"
-tpu midGPT ssh node3 "sudo mount -o discard,defaults /dev/sdb /mnt/disks/persist"
+tpu midGPT ssh $2 "sudo mkdir -p /mnt/disks/persist"
+tpu midGPT ssh $2 "sudo mount -o discard,defaults /dev/sdb /mnt/disks/persist"
