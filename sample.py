570f774edadc2fac2b30af791ecd22a161d8e092
Nicholas C. Landolfi <lando@stanford.edu>
2023-12-13 07:24:56 +0000

    fix sample.py (could still use cleanup)

--- sample.py
+++ sample.py
@@ -3,19 +3,23 @@ Sample from a trained model
 """
 import argparse
 import os
+import json
+import pickle
+
+from jax.experimental import mesh_utils
+from src.model import GPT
+from src.train import ExperimentConfig
 import equinox as eqx
-import numpy as np
+import gcsfs
 import jax
 import jax.numpy as jnp
-from jax.experimental import mesh_utils
 import jax.random as jrandom
-import jmp
-import json
+import numpy as np
+import optax  # type: ignore
 import orbax.checkpoint as ocp
-import pickle
-from src.train import ExperimentConfig
-from src.model import GPT
 import tiktoken
+
+from src.train import cast_pytree
 
 jtu = jax.tree_util
 NamedSharding, Mesh = jax.sharding.NamedSharding, jax.sharding.Mesh
@@ -31,8 +35,18 @@ parser.add_argument("--num_samples", type=int, default=10)
 parser.add_argument("--max_new_tokens", type=int, default=500)
 parser.add_argument("--temperature", type=float, default=0.8)
 # retain only the top_k most likely tokens, clamp others to have 0 probability
+# TODO: implement
 parser.add_argument("--top_k", type=int, default=200)
 cmd_args = parser.parse_args()
+
+if cmd_args.ckpt_dir.startswith("gs://"):
+    print("Using GCS filesystem for checkpoint")
+    fs = gcsfs.GCSFileSystem()
+    fopen = fs.open
+else:
+    print("Using local filesystem for checkpoint")
+    fs = os
+    fopen = open
 
 
 def from_json(json_path, dataclass_type):
@@ -49,19 +63,19 @@ def from_json(json_path, dataclass_type):
         else:
             return dict_or_list
 
-    with open(json_path, "r") as f:
+    with fopen(json_path, "r") as f:
         json_string = f.read()
     return convert(json.loads(json_string), dataclass_type)
 
 
-def generate(model, idx, max_new_tokens, temperature=1.0, top_k=None, key=None):
-    block_size = model.wpe.weight.shape[0]  # TODO: get this from config.
+def generate(config, model, idx, max_new_tokens, temperature=1.0, top_k=None, key=None):
+    block_size = config.model_config.block_size
     # TODO: Move JIT outside this function?
     batched_model = eqx.filter_jit(jax.vmap(eqx.Partial(model, inference=True)))
     for _ in range(max_new_tokens):
         # take the final block_size tokens for conditioning, if the sequence is too long
         idx_cond = idx if idx.shape[1] <= block_size else idx[:, -block_size:]
-        pluck_T = idx.shape[1]-1
+        pluck_T = idx.shape[1] - 1
         if idx_cond.shape[1] < block_size:
             B, pad_T = idx_cond.shape[0], block_size - idx_cond.shape[1]
             padding = jnp.zeros((B, pad_T), dtype=idx_cond.dtype)
@@ -75,7 +89,9 @@ def generate(model, idx, max_new_tokens, temperature=1.0, top_k=None, key=None):
         # TODO: handle top_k
         key, next_token_key = jrandom.split(key)
         # sample from the distribution
-        idx_next = jax.random.categorical(next_token_key, logits, axis=1, shape=(idx.shape[0], 1))
+        idx_next = jax.random.categorical(
+            next_token_key, logits, axis=1, shape=(idx.shape[0], 1)
+        )
         # append sampled index to the running sequence and continue
         idx = jnp.concatenate([idx, idx_next], axis=1)
     return idx
@@ -86,26 +102,51 @@ config_path: str = os.path.join(cmd_args.ckpt_dir, "config.json")
 config: ExperimentConfig = from_json(config_path, ExperimentConfig)
 eqx.tree_pprint(config)
 
-options = ocp.CheckpointManagerOptions(
-    max_to_keep=1, save_interval_steps=config.eval_interval
-)
 mngr = ocp.CheckpointManager(
-    os.path.abspath(os.path.join(config.rundir, "ckpt_mngr")),
+    config.rundir,
     ocp.PyTreeCheckpointer(),
-    options=options,
 )
-model_leaves, _opt_state = mngr.restore(mngr.latest_step())
+# model_leaves, _opt_state = mngr.restore(mngr.latest_step())
+# model = GPT(config.model_config, key=jrandom.PRNGKey(0))
+# model: GPT = jtu.tree_unflatten(jtu.tree_structure(model), model_leaves)
+
 model = GPT(config.model_config, key=jrandom.PRNGKey(0))
-model: GPT = jtu.tree_unflatten(jtu.tree_structure(model), model_leaves)
+
+# both of these are unused, but just for loading the checkpoint
+scheduler = optax.warmup_cosine_decay_schedule(
+    0,
+    config.learning_rate,
+    config.warmup_steps,
+    config.lr_decay_steps,
+    end_value=config.min_lr,
+)
+optimizer = optax.chain(
+    optax.clip_by_global_norm(1.0),
+    optax.scale_by_adam(b2=config.beta2),
+    optax.add_decayed_weights(config.weight_decay / config.learning_rate),
+    optax.scale_by_schedule(scheduler),
+    optax.scale(-1),
+)
+
+#
+opt_state = optimizer.init(eqx.filter(model, eqx.is_array))
+ex_state = (jtu.tree_leaves(model), jtu.tree_leaves(opt_state))
+ex_shardings = jtu.tree_map(lambda x: x.sharding if eqx.is_array(x) else None, ex_state)
+restore_args = ocp.checkpoint_utils.construct_restore_args(ex_state, ex_shardings)
+model_leaves, opt_state_leaves = mngr.restore(
+    mngr.latest_step(), restore_kwargs={"restore_args": restore_args}
+)
+model = jtu.tree_unflatten(jtu.tree_structure(model), model_leaves)
 
 # set up encoding/decoding
 # the next several lines are copied directly from nanoGPT
 # look for the meta pickle in case it is available in the dataset folder
+# only for shakespeare_char
 load_meta = False
 meta_path = os.path.join(config.data_dir, "meta.pkl")
 load_meta = os.path.exists(meta_path)
 if load_meta:
-    print(f"Loading meta from {meta_path}...")
+    print(f"Loading meta from LOCAL {meta_path}...")
     with open(meta_path, "rb") as f:
         meta = pickle.load(f)
     # TODO want to make this more general to arbitrary encoder/decoder schemes
@@ -114,7 +155,7 @@ if load_meta:
     decode = lambda l: "".join([itos[i] for i in l])
 else:
     # ok let's assume gpt-2 encodings by default
-    print("No meta.pkl found, assuming GPT-2 encodings...")
+    print("No LOCAL meta.pkl found, assuming GPT-2 encodings...")
     enc = tiktoken.get_encoding("gpt2")
     encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
     decode = lambda l: enc.decode(l)
@@ -137,11 +178,12 @@ jax.debug.visualize_array_sharding(x)
 jax.debug.visualize_array_sharding(model.lm_head.weight_MxN)
 
 print("generating samples...")
-model = jmp.get_policy(config.policy).cast_to_compute(model)
+model = cast_pytree(model, jnp.dtype(config.compute_dtype))
 key = jrandom.PRNGKey(0)
 for _ in range(cmd_args.num_samples):
     key, sample_key = jrandom.split(key)
     y = generate(
+        config,
         model,
         x,
         cmd_args.max_new_tokens,
